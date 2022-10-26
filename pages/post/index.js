8bf9abc8e869b3654298d0355ef79eac0973f1cd
Anderson Keller <andersonkellervendas@hotmail.com>
2022-10-26 14:33:00 -0300

    fix:buttons

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,7 +1,7 @@
 /* Desenvolva seu script aqui */
-import { getLocalStorage } from "../../scripts/localStorage.js";
+import { getLocalStorage, getIdLocalStorage } from "../../scripts/localStorage.js";
 import { renderButtons } from "../home/index.js";
-const localNews = await getLocalStorage()
+import {getPostById} from "../../scripts/requests.js"
 await renderButtons();
 
 function goToHome(){
@@ -11,3 +11,25 @@ function goToHome(){
     })
 }
 goToHome()
+
+async function renderPostSelected(){
+    const id = await getIdLocalStorage();
+    console.log(id)
+    const post = await getPostById(id);
+    console.log(post)
+
+    const main = document.querySelector("main")
+    main.insertAdjacentHTML("afterbegin",`
+    <section class="section-post">
+            <div class="div-post-header">
+                <h2 class="title-main title1">${post.title}</h2>
+        <p class="p-description text1">${post.description}</p>
+            </div>
+        <div class="div-post-body">
+            <img src="${post.image}" alt="${post.title}">
+        <p class="p-content text2">${post.content}</p> 
+        </div>
+        </section>
+    `)
+}
+renderPostSelected()
--- scripts/requests.js
+++ scripts/requests.js
@@ -6,3 +6,10 @@ export async function conectAPI(){
     localStorage.setItem("newsList",JSON.stringify(dataJson.news))
     return dataJson;
 }
+export async function getPostById(id){
+    const baseUrl = "https://m2-api-living.herokuapp.com/news/";
+    const data = await fetch(`${baseUrl}${id}`);
+    const dataJson = await data.json();
+
+    return dataJson;
+}
